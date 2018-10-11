53cbc8c9cde096c01f15772f8226469bc6145d2a
Julian Mathias Kock <juli516b@edu.eal.dk>
2018-10-11 22:26:38 +0200

    Implementeret endnu mere.

--- WelfareDenmark/Startup.cs
+++ WelfareDenmark/Startup.cs
@@ -21,13 +21,14 @@ namespace WelfareDenmark
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=index}/{id?}");
             });
-            app.UseFileServer();
         }
 
         
---
+++ WelfareDenmark/ViewModels/AddressDescriptionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WelfareDenmark.ViewModels
+{
+    public class AddressDescriptionViewModel
+    {
+        [MinLength(0)]
+        [MaxLength(1000)]
+        public string AddressDesc { get; set; }
+    }
+}
